b19d0351a2c7615e59bd8ddbf4eb714b50a463d3
Lucas <lucas.r.frank@gmail.com>
2019-04-03 17:51:26 -0300

    Consertando um erro

--- analisys.py
+++ analisys.py
@@ -29,6 +29,15 @@ def coefficientOfVariation(data):
     coefficientOfVariationValue = standardDeviation(data) / mean(data)
     return coefficientOfVariationValue
 
+def mode(data):
+    df = data.groupby(data).size().reset_index(name='count')
+    df = np.array(df.sort_values(["count"]))
+    modeValue = df[-1][0]
+    modeFrequence = df[-1][1]
+    print(df)
+    return modeValue,modeFrequence
+
+
 def median(data):
     data = np.sort(data)
     medianValue = 0
@@ -89,10 +98,26 @@ def cdf(data):
     plt.plot(x,y, label = 'CDF')
     plt.show()
 
+name = "fileSizeComplete.txt"
+if name == "fileSizeComplete.txt":
+    columns = ["Size"]
+else:
+    columns = ["Quantity","Size"]
+df = pd.read_csv(name, delim_whitespace = True, header = None, names=columns)
 
-df = pd.read_csv("fileSize.txt", delim_whitespace = True, header = None, names=["Quantity","Size"])
-
+sorteddf = np.sort(df['Size'])
+valorDaModa,frequencia = mode(df["Size"])
+print("A moda dos tamanhos de arquivos é: {} e o número de vezes que ela apareceu foi de: {} ".format(valorDaModa,frequencia))
+exit(0)
+print(df)
 print(mean(df['Size']))
 print(variance(df['Size']))
 print(standardDeviation(df['Size']))
 print(coefficientOfVariation(df['Size']))
+print(median(df['Size']))
+print(quartile(1,df['Size']))
+print(quartile(2,df['Size']))
+print(quartile(3,df['Size']))
+print(interquartileRange(df['Size']))
+pdf(df['Size'])
+cdf(df['Size'])
